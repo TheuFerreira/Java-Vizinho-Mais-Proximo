1a48a4957ea18ceec84bd4fe41ad3df1cc4f0374
Ferreira <matheus.44300@gmail.com>
2021-02-10 16:31:51 -0300

    Update Main.java

--- src/mth/trabalho/Main.java
+++ src/mth/trabalho/Main.java
@@ -9,26 +9,26 @@ public class Main {
     public static void main(String[] args) {
 	    // write your code here
 
-        int path[][] = readFile("paulo_bebedor.txt");
+        int[][] path = readFile();
 
-        for (int i = 0; i < path.length; i++) {
-            for (int j = 0; j < path[i].length; j++) {
-                System.out.print(path[i][j] + " ");
+        for (int[] i : path) {
+            for (int j : i) {
+                System.out.print(j + " ");
             }
-            System.out.println("");
+            System.out.println();
         }
 
         List<Integer> minPath = nearestNeighbor(path);
-        for (int i = 0; i < minPath.size(); i++) {
-            System.out.print(minPath.get(i) + " ");
+        for (Integer i : minPath) {
+            System.out.print(i + " ");
         }
-        System.out.println("");
+        System.out.println();
     }
 
-    private static int[][] readFile(String path) {
-        int matrix[][] = new int[0][0];
+    private static int[][] readFile() {
+        int[][] matrix = new int[0][0];
         try {
-            File file = new File(path);
+            File file = new File("paulo_bebedor.txt");
             Scanner scanner = new Scanner(file);
 
             int size = Integer.parseInt(scanner.nextLine());
@@ -37,25 +37,24 @@ public class Main {
             for (int i = 0; i < size; i++) {
                 String line = scanner.nextLine();
 
-                String values[] = line.split(" ");
+                String[] values = line.split(" ");
                 for (int j = 0; j < values.length; j++) {
                     matrix[i][j] = Integer.parseInt(values[j]);
                 }
             }
         } catch (FileNotFoundException ex) {
             System.out.println(ex.toString());
-        } finally {
-            return matrix;
         }
+        return matrix;
     }
 
     // Vizinho mais próximo
-    private static List<Integer> nearestNeighbor(int matrix[][]) {
+    private static List<Integer> nearestNeighbor(int[][] matrix) {
         List<Integer> path = new ArrayList<>();
         path.add(0);
 
         int currentPosition = path.get(0);
-        while (true) {
+        while (path.size() != matrix.length) {
             int selectedIndex = -1;
             int selectedWeight = -1;
             for (int i = 0; i < matrix[currentPosition].length; i++) {
@@ -75,8 +74,6 @@ public class Main {
             }
             currentPosition = selectedIndex;
             path.add(selectedIndex);
-            if (path.size() == matrix.length)
-                break;
         }
 
         path.add(0);
