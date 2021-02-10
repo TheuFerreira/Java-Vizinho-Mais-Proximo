ed7913fd442bf1465a223472a6328109efda83cc
Ferreira <matheus.44300@gmail.com>
2021-02-10 15:32:01 -0300

    Reading a file

--- src/mth/trabalho/Main.java
+++ src/mth/trabalho/Main.java
@@ -1,8 +1,45 @@
 package mth.trabalho;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
+
+        int path[][] = ReadFile("paulo_bebedor.txt");
+
+        for (int i = 0; i < path.length; i++) {
+            for (int j = 0; j < path[i].length; j++) {
+                System.out.print(path[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    private static int[][] ReadFile(String path) {
+        int matrix[][] = new int[0][0];
+        try {
+            File file = new File(path);
+            Scanner scanner = new Scanner(file);
+
+            int size = Integer.parseInt(scanner.nextLine());
+            matrix = new int[size][size];
+
+            for (int i = 0; i < size; i++) {
+                String line = scanner.nextLine();
+
+                String values[] = line.split(" ");
+                for (int j = 0; j < values.length; j++) {
+                    matrix[i][j] = Integer.parseInt(values[j]);
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.toString());
+        } finally {
+            return matrix;
+        }
     }
 }
